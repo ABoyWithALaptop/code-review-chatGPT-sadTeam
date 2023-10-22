e3031b1d6c8c558170e62e02340dd0424913f98f
Tu Pham <tupx1204@gmail.com>
2023-10-22 17:56:44 +0700

    fix show diff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 "use client";
-import { getDiff, getPulls, pull } from "@/api/github";
-import Image from "next/image";
-import { useState } from "react";
+
 import SelectRepo from "@/components/SelectRepo";
-import ListPR from "@/components/ListPR";
 
 export default function Home() {
-	return (
-		<SelectRepo />
-	)
+	return <SelectRepo />;
 }
